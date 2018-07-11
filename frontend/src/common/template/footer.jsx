ac090e94d20e3f460e0e3a5e9ad2deead6441c81
Gustavo Zatt Heidemann <gustavo@catamoeda.com.br>
2018-07-11 18:43:59 -0300

    - Section 9 Class 121

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props =>(
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2018
+            <a href='https://github.com/gustavozh' target='_blank'> Gustavo's Git</a>.
+        </strong>
+    </footer>
+)
